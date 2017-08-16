7d6282e2822fc6a417f53f861ea3f71c9729d987
Scott J Miles <sjmiles@google.com>
2017-08-16 11:55:27 -0700

    migrate utilities out of ArcsLib.js into utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,35 @@
 // ad-hoc (for now) utilities
 let utils = {
+  createArc: ({id, urlMap, slotComposer}) => {
+    // worker paths are relative to worker location, remap urls from there to here
+    let remap = utils._expandUrls(urlMap);
+    // Configure worker factory
+    let pecFactory = utils._createPecWorker.bind(null, urlMap[`worker-entry-cdn.js`], remap);
+    // create an arc
+    return new Arcs.Arc({id, pecFactory, slotComposer});
+  },
+  _expandUrls: urlMap => {
+    let remap = {};
+    Object.keys(urlMap).forEach(k => {
+      let path = urlMap[k];
+      if (path[0] === '/') {
+        path = `${location.origin}${path}`;
+      } else if (path.indexOf('//') < 0) {
+        let root = location.href.split('/').slice(0, -1).join('/');
+        //console.log(`expanding relative path [${path}] to [${root}/${path}]`);
+        path = `${root}/${path}`;
+      }
+      remap[k] = path;
+    });
+    return remap;
+  },
+  _createPecWorker: (path, map, id) => {
+    let channel = new MessageChannel();
+    //let worker = new Worker(path);
+    let worker = new Worker(URL.createObjectURL(new Blob([`importScripts("${path}");`])));
+    worker.postMessage({id: `${id}:inner`, base: map}, [channel.port1]);
+    return channel.port2;
+  },
   createUrlMap: cdnRoot => {
     return {
       // TODO(sjmiles): mapping root and dot-root allows browser-cdn-loader to replace right-hand
@@ -31,7 +61,8 @@ let utils = {
   },
   suggest: async (arc, ui, planner, recipes) => {
     planner.init(arc, {arc, recipes});
-    let suggestions = await planner.suggest(500);
+    let generations = [];
+    let suggestions = await planner.suggest(5000, generations);
     suggestions.forEach((suggestion, i) => ui.add(suggestion, i));
   }
 };
