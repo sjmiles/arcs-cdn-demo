7c46242de79b7b82fd94671615bc6d2b271ab95b
Scott J Miles <sjmiles@google.com>
2017-08-17 16:58:54 -0700

    fix manifest, simplify db

--- db.js
+++ db.js
@@ -1,4 +1,14 @@
-window.db = {
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+ window.db = {
   views: [
     {
       name: "accounts",
@@ -17,14 +27,6 @@ window.db = {
           balance: "999.99"
         }
       ]
-    }/*,
-    {
-      name: "selected",
-      type: "Account",
-      model: [{
-        name: "",
-        balance: "0"
-      }]
-    }*/
+    }
   ]
 };
